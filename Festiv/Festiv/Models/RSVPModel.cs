fe5b2e28325a6e437676a9caa7c79d1ab1bee889
kavitha1808 <kavitha.sekarp@gmail.com>
2024-07-02 14:49:47 -0500

    hello

---
+++ Festiv/Festiv/Models/RSVPModel.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Festiv.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PartyRsvp.Models
+{
+    public class GuestRespond
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? Attend { get; set; }
+
+    }
+}
