01921b67784b78e6609abd250b176c64221254ba
JC <marybecker053@gmail.com>
2024-07-02 20:09:49 -0500

    Game Model

---
+++ Festiv/Models/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Festiv.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<User> Users { get; set; } 
+
+        public List<User> CurrentPlayers { get; set; }
+
+        public List<List<User> Teams { get; set; }
+
+        public Game(string name, List<User> currentPlayers, List<User>teams)
+        {
+            Name = name;
+            CurrentPlayers =  new List<currentPlayers>();
+            Teams = new List<teams>();
+        }
+
+        public Game()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Game @game && 
+                Id == @game.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+    }
+}
