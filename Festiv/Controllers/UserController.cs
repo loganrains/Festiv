cf2262c0c26f88daf98848f672b3917129e707e2
Tucker Overmann <tuckerovermann@gmail.com>
2024-07-03 14:35:00 -0500

    Added identity to user class

--- Festiv/Controllers/UserController.cs
+++ Festiv/Controllers/UserController.cs
@@ -1,6 +1,20 @@
-﻿namespace Festiv.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Festiv.Models;
+using Microsoft.AspNetCore.Identity;
 
-public class UserController
+namespace Festiv.Controllers;
+
+public class UserController : Controller
 {
+    private readonly Festiv.Models.User _user;
 
+    public UserController(Festiv.Models.User user)
+    {
+        _user = user;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
 }
--- Festiv/Models/User.cs
+++ Festiv/Models/User.cs
@@ -1,24 +1,32 @@
-﻿namespace Festiv.Models;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class User
+namespace Festiv.Models;
+
+
+//No Id because need to properly extend identityuser and update DbContext//
+
+public class User : IdentityUser
 {
-    public int Id {get; set;}
-    public string? Name {get; set;}
-    public string? Email {get; set;}
     public string? FirstName {get; set;}
     public string? LastName {get; set;}
     public string? ProfilePic {get; set;}
-    public int? Rating {get; set;}
+    public int Rating {get; set;}
     public bool UserType {get; set;}
-    public User(int id, string? name, string? email, string? firstName, string? lastName, string? profilePic, int? rating, bool userType)
+    public User(bool userType, string? firstName, string? lastName, string? profilePic, int rating = 1000)
     {
-        Id = id;
-        Name = name;
-        Email = email;
+
         FirstName = firstName;
         LastName = lastName;
         ProfilePic = profilePic;
         Rating = rating;
         UserType = userType;
+    }
+        public User()
+    {
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id);
     }
 }
