1b69091f2f50d0ec9f7523ac909f9f0ae94e087d
loganrains <logankrains@gmail.com>
2024-07-17 11:00:43 -0500

    Added name to party details

---
+++ Festiv/Migrations/20240716203113_PartyMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Festiv.Migrations
+{
+    /// <inheritdoc />
+    public partial class PartyMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
