f54a78d5643b433c174e7bc041f28c30b760ea9f
JC <marybecker053@gmail.com>
2024-07-19 12:44:29 -0500

    About to pull develpoment

--- Festiv/Services/SpotifyAccountService.cs
+++ Festiv/Services/SpotifyAccountService.cs
@@ -1,29 +1,30 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using SpotifyAPI.Web;
-// using System.Net.Http.Headers;
-// using System.Reflection.Metadata.Ecma335;
-// using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
 
-// namespace Festiv.Services
-// {
-//     public class SpotifyAccountService : ISpotifyAccountService
-//     {
-//         public readonly HttpClient _httpClient;
+namespace Festiv.Services
+{
+    public class SpotifyAccountService : ISpotifyAccountService
+    {
+        public readonly HttpClient _httpClient;
 
-//         public SpotifyAccountService(HttpClient httpClient)
-//         {
-//             _httpClient = httpClient;
-//         }
+        public SpotifyAccountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
-//         public Task<string> GetToken(string clientId, string clientSecret)
-//         {
-//             var request = HttpRequestMessage(HttpMethod.Post, "token");
+        public Task<string> GetToken(string clientId, string clientSecret)
+        {
+            throw new NotImplementedException();
+            // var request = HttpRequestMessage(HttpMethod.Post, "token");
 
-//             request.Headers.Authorization = new AuthenticationHeaderValue(
-//                 "Basic", Convert.Tobase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
-//         }
-//     }
-// }
+            // request.Headers.Authorization = new AuthenticationHeaderValue(
+            //     "Basic", Convert.Tobase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
+        }
+    }
+}
